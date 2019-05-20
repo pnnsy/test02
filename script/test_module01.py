f190360151bbe77a7cf20b70402b74b5fbcb0b8f
nana <nana@163.com>
2019-05-20 16:15:13 +0800

    提交脚本

---
+++ script/test_module01.py
@@ -0,0 +1,10 @@
+# --html / report.html
+import pytest
+# pytestmark = pytest.mark.skip()
+class TestDemo:
+
+    def test_001(self):
+        print("我是测试函数1")
+    def test_002(self):
+        print("我是测试函数2")
+
